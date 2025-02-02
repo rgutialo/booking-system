35e2f244b00e639e1c9709a67a56086164e25e73
Raul Gutierrez <raul.gutialo@gmail.com>
2025-02-02 21:20:33 +0100

    Added API Gateway to project

--- payment-service/src/main/java/com/algerage/payment/infraestructure/adapter/in/rest/PaymentController.java
+++ payment-service/src/main/java/com/algerage/payment/infraestructure/adapter/in/rest/PaymentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/payment")
+@RequestMapping(value = "/payments")
 public class PaymentController {
 
     private final RandomGeneratorPort randomGenerator;

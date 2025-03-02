0ece188862f42003d763b5bf797217873817de4b
Raul Gutierrez <raul.gutialo@gmail.com>
2025-03-02 10:33:48 +0100

    Added local keycloak when testing locally services

--- booking-service/src/main/java/com/algerage/booking/infraestructure/config/SecurityConfig.java
+++ booking-service/src/main/java/com/algerage/booking/infraestructure/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.algerage.booking.infraestructure.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,14 +23,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
         http
                 .authorizeHttpRequests(auth -> auth
                         //.requestMatchers("/admin/**").hasRole("admin")
+                        .requestMatchers(HttpMethod.POST, "/booking").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2
                         //.jwt(Customizer.withDefaults()));
                         .jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+
+        http.sessionManagement(
+                t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
         return http.build();
     }
 }
